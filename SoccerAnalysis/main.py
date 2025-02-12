fe898aa535419f1e00d0c7bee284e3fc3a757c3d
kristal <shresthakristal42@gmail.com>
2025-02-13 00:25:02 +0545

    project

---
+++ SoccerAnalysis/main.py
@@ -0,0 +1,55 @@
+from utils import read_video, save_video
+from tracker import Tracker
+import cv2
+from team_assigner import TeamAssigner
+from player_ball_assigner import PlayerBallAssigner
+from camera_movement_estimator import CameraMovementEstimator
+import numpy as np
+def main():
+    #read video
+    video_frames=read_video(r"C:\Users\Kristal\Desktop\Colab_Notebooks\SoccerAnalysis\input_videos\08fd33_4.mp4")
+
+    #Initialize tracker
+    tracker=Tracker(r'C:\Users\Kristal\Desktop\Colab_Notebooks\SoccerAnalysis\model\best.pt')
+    tracks =tracker.get_objects_tracks(video_frames,read_from_stub=True,stub_path=r'C:\Users\Kristal\Desktop\Colab_Notebooks\SoccerAnalysis\stubs\track_stubs.pkl')
+
+    #camera movement estimator
+    camera_movement_estimator=CameraMovementEstimator(video_frames[0])
+    camera_movement_per_frame=camera_movement_estimator.get_camera_movement(video_frames,read_from_stub=True,stub_path=r'C:\Users\Kristal\Desktop\Colab_Notebooks\SoccerAnalysis\stubs\camera_movement_stubss.pkl')
+
+    #interpolate Ball positions
+    tracks['ball']=tracker.interpolate_ball_positions(tracks['ball'])
+    #assign player teams
+    team_assigner=TeamAssigner()
+    team_assigner.assign_team_color(video_frames[0],tracks['players'][0])
+
+    for frame_num,player_track in enumerate(tracks['players']):
+      for player_id,track in player_track.items():
+        team=team_assigner.get_player_team(video_frames[frame_num],track['bbox'],player_id)
+        tracks['players'][frame_num][player_id]['team']=team
+        tracks['players'][frame_num][player_id]['team_color']=team_assigner.team_colors[team]
+
+    # assign ball acquisition
+    player_assigner=PlayerBallAssigner()
+    team_ball_control=[]
+    for frame_num,player_track in enumerate(tracks['players']):
+      ball_bbox=tracks['ball'][frame_num][1]['bbox']
+      assigned_player=player_assigner.assign_ball_to_player(player_track,ball_bbox)
+
+      if assigned_player != -1:
+        tracks['players'][frame_num][assigned_player]['has_ball']=True
+        team_ball_control.append(tracks['players'][frame_num][assigned_player]['team'])
+      else:
+         team_ball_control.append(team_ball_control[-1])
+    team_ball_control=np.array(team_ball_control)
+
+    #draw output
+    output_video_frames=tracker.draw_annotations(video_frames,tracks,team_ball_control)
+
+    #draw camera movement
+    output_video_frames=camera_movement_estimator.draw_camera_movement(output_video_frames,camera_movement_per_frame)
+    #save video
+    save_video(output_video_frames, r"C:\Users\Kristal\Desktop\Colab_Notebooks\SoccerAnalysis\output_videos/output_video(CameraMovement123).avi")
+
+if __name__ == "__main__":
+    main()
